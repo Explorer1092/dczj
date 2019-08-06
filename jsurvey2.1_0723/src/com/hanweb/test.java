a92b5f964fef3fb401da67295b63a96a478b3884
chenxianfeng <gaoyan20chen@foxmail.com>
2019-08-06 18:33:58 +0800

    1

---
+++ jsurvey2.1_0723/src/com/hanweb/test.java
@@ -0,0 +1,41 @@
+package com.hanweb;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[][] filetypeStrings = 
+			{
+			{"限制上传文件大小：","4","0"},
+			{"限制上传文件类型：","1"},
+			{"图片文件：","2"},
+			{".gif","7"},
+			{".png","7"},
+			{".jpg","7"},
+			{".jpeg","7"},
+			{"文档文件：","3"},
+			{".doc","8"},
+			{".docx","8"},
+			{".pdf","8"},
+			{".xls","8"},
+			{".xlsx","8"},
+			{".ppt","8"},
+			{".pptx","8"},
+			{".txt","8"},
+			{"压缩文件：","4"},
+			{".rar","9"},
+			{".zip","9"},
+			{"视频文件：","5"},
+			{".mp4","10"},
+			{".avi","10"},
+			{".mov","10"},
+			{"音频文件：","6"},
+			{".mp3","11"},
+			{".amr","11"},
+			{".waw","11"}
+			};
+		System.out.println(filetypeStrings.length);
+
+	}
+
+}
